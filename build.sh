ef1795f0e2bac49c2c62737d9080c4baa46829b9
Philippe Voinov <philippevoinov@gmail.com>
2021-09-28 16:37:25 +0200

    enable build

--- build.sh
+++ build.sh
@@ -3,12 +3,12 @@
 trap "kill 0" EXIT
 
 cd open-data-backend
-# python -m src.preprocess
+python -m src.preprocess
 uvicorn main:app &
 python -m src.static
 
 cd ../open-data-frontend
-# REACT_APP_API_URL='https://ask-open-data.ch/api' yarn build
+REACT_APP_API_URL='https://ask-open-data.ch/api' yarn build
 
 cd ..
 rm -rf build

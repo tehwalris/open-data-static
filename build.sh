c6471517146f90b7b3ea090f7b5ad99a83523f70
Philippe Voinov <philippevoinov@gmail.com>
2021-09-28 16:33:12 +0200

    stuff

--- build.sh
+++ build.sh
@@ -1,8 +1,17 @@
 #!/usr/bin/env bash
 
+trap "kill 0" EXIT
+
 cd open-data-backend
-python -m src.preprocess
+# python -m src.preprocess
+uvicorn main:app &
+python -m src.static
 
 cd ../open-data-frontend
-# TODO env
-yarn build
+# REACT_APP_API_URL='https://ask-open-data.ch/api' yarn build
+
+cd ..
+rm -rf build
+cp -R open-data-frontend/build build
+cp -R open-data-backend/static build/api
+ls -lah build
